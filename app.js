c8fefba927c604f419391ee06a59dd794e9b1d32
Morgan LaCouture <morganlea18@gmail.com>
2017-08-02 16:45:06 -0700

    provide question for guessing game

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 var name = prompt ('Hi! What is your name?');
-console.log ('Hi! What is your name?');
 
-alert ('Wow! ' + name + ' is a really good name!'); 
-console.log ('Wow! ' + name + ' is a really good name!');
+// alert ('Wow! ' + name + ' is a really good name!'); 
 
-var color = prompt ( name + ', is green my favorite color?').toLowerCase();
-console.log ( name + ', is green my favorite color? ');
+// var color = prompt ( name + ', is green my favorite color?').toLowerCase();
+// console.log ( name );
 
 // if (color === 'yes') {
 //      alert ('YOU ARE CORRECT!')
@@ -17,38 +15,62 @@ console.log ( name + ', is green my favorite color? ');
 //      alert ('Oh no, green IS my favorite color!')
 // }; 
 
-if (color === 'yes' | color === 'y') {alert ('You are correct!')}
-else if (color === 'no' | color === 'n') {alert ('Oh no, green IS my favorite color!')};
+// if (color === 'yes' | color === 'y') {alert ('You are correct!');}
+// else if (color === 'no' | color === 'n') {alert ('Oh no, green IS my favorite color!');};
 
-var pets = prompt ('Do I own cats or dogs?').toLowerCase();
-console.log ('Do I own cats or dogs?');
+// var pets = prompt ('Do I own cats or dogs?').toLowerCase();
+// console.log ('Do I own cats or dogs?');
 
-if (pets === 'cats') {alert ('You are correct! I own two beautiful babies.')}
-else if (pets === 'dogs') {alert ('Oh no, I do own cats!')};
+// if (pets === 'cats') {alert ('You are correct! I own two beautiful babies.');}
+// else if (pets === 'dogs') {alert ('Oh no, I do own cats!');};
 
-var food = prompt ('Do I eat Thai food almost every day of my life?').toLowerCase();
-console.log ('Do I eat Thai food almost every day of my life?');
+// var food = prompt ('Do I eat Thai food almost every day of my life?').toLowerCase();
+// console.log ('Do I eat Thai food almost every day of my life?');
 
-if (food === 'yes' | food === 'y') {alert ('Why yes I do!')}
-else if (food === 'no' | food === 'n') {alert ('I do! Thai food is my favorite.')};
+// if (food === 'yes' | food === 'y') {alert ('Why yes I do!');}
+// else if (food === 'no' | food === 'n') {alert ('I do! Thai food is my favorite.');};
 
-var music = prompt ('Do I listen to a lot of music?').toLowerCase();
-console.log ('Do I listen to a lot of music?');
+// var music = prompt ('Do I listen to a lot of music?').toLowerCase();
+// console.log ('Do I listen to a lot of music?');
 
-if (music === 'yes' | music === 'y') {alert ('Correct! Music is my favorite!')}
-else if (music === 'no' | music === 'n') {alert ('What! Of course I do!')};
+// if (music === 'yes' | music === 'y') {alert ('Correct! Music is my favorite!');}
+// else if (music === 'no' | music === 'n') {alert ('What! Of course I do!');};
 
-var plants = prompt ('Do I keep a lot of plants in my house?').toLowerCase();
-console.log ('Do I keep a lot of plants in my house?');
+// var plants = prompt ('Do I keep a lot of plants in my house?').toLowerCase();
+// console.log ('Do I keep a lot of plants in my house?');
 
-if (plants === 'yes' | plants === 'y') {alert ('Yes I do! Plants are the best.')}
-else if (plants === 'no' | plants === 'n') {alert ('You are wrong! I love plants.')};
+// if (plants === 'yes' | plants === 'y') 
+// {alert ('Yes I do! Plants are the best.');
+// }
 
-var tears = prompt ('Was I born without tearducts?').toLowerCase();
-console.log ('Was I born without tearducts?');
+// else if (plants === 'no' | plants === 'n') {alert ('You are wrong! I love plants.');};
 
-if (tears === 'yes' | tears === 'y') {alert ('You are correct!')}
-else if (tears === 'no' | tears === 'n') {alert ('As a matter of fact, I was born without tearducts.')};
+// var tears = prompt ('Was I born without tear ducts?').toLowerCase();
+// console.log ('Was I born without tear ducts?');
 
-alert ('Thank you for answering my questions!');
+// if (tears === 'yes' | tears === 'y') {alert ('You are correct!');}
+// else if (tears === 'no' | tears === 'n') {alert ('As a matter of fact, I was born without tear ducts.');};
 
+// alert ('Thank you for answering my questions!');
+
+// var states = ['idaho', 'utah', 'colorado'];
+// console.log ('guess');
+
+// do {
+//     var guess = prompt ( name + ', can you guess a state i\'ve lived in?').toLowerCase();
+//     console.log(guess);
+// }
+// while (!states.indexOf ( guess ));  
+var states = ['idaho', 'utah', 'colorado'];
+
+for (  var i = 6 ; i > 0 ; i--) {
+   
+     var guess = prompt ( name + ', can you guess what state i\'ve lived in? You have ' + i + ' guesses')
+   
+    if ( states.indexOf(guess) !== -1 ) {
+        alert ('Good job!'); 
+         break;
+    }
+
+    else {alert ('Sorry!');}
+}
