2e7e1d112447b70165c3d95128d6c5c6aa8ac21e
FrankSeaman <frankseamen@gmail.com>
2017-08-03 16:43:33 -0700

    ML lab 4

--- app.js
+++ app.js
@@ -4,105 +4,29 @@ alert ('Wow! ' + name + ' is a really good name!');
 
 var tally = 0;
 
-var color = prompt ( name + ', is green my favorite color?').toLowerCase();
-console.log ( name );
+function question (promptMessage, correctAnswer, alertMessage, wrongAnswer){
+    console.log(correctAnswer);
+    var input = prompt (promptMessage).toLowerCase();
 
-if (color === 'yes' | color === 'y') {
+    if ( input === correctAnswer || input === correctAnswer.charAt(0)  ) {
+        tally = tally + 1;
 
-    tally = tally + 1;
+        alert (alertMessage);
+    }else{
 
-    alert ('You are correct!');
+        alert (wrongAnswer);
+    }
 }
+question('Is Green my favorite color?', 'yes', 'You are Correct', 'Oh no, Green is my favorite color' );
+question(' Do I eat Thai food everyday of my life?', 'yes', 'Why yes I do', 'I do Thai food is my favorite');
+question('Do I listen to a lot of music?', 'yes', 'Correct', 'What!!! of course I do');
+question(' Do I keep a lot of plants in my house?', 'yes', 'Yes I Do', 'You are Wrong I love plants');
+question(' Was i born without tear ducts?', 'yes', 'YOu are correct', 'you are wrong');
 
-else if (color === 'no' | color === 'n') {
-
-    alert ('Oh no, green IS my favorite color!');
-}
-
-var pets = prompt ('Do I own cats or dogs?').toLowerCase();
-console.log ('Do I own cats or dogs?');
-
-if (pets === 'cats') {
-
-    tally = tally + 1;
-
-    alert ('You are correct! I own two beautiful babies.');
-
-}
-else if (pets === 'dogs') {
-
-    alert ('Oh no, I do own cats!');
-}
-
-var food = prompt ('Do I eat Thai food almost every day of my life?').toLowerCase();
-console.log ('Do I eat Thai food almost every day of my life?');
-
-if (food === 'yes' | food === 'y') {
-    
-    tally = tally + 1;
-
-    alert ('Why yes I do!');
-}
-
-else if (food === 'no' | food === 'n') {
-    
-    alert ('I do! Thai food is my favorite.');
-
-}
-
-var music = prompt ('Do I listen to a lot of music?').toLowerCase();
-console.log ('Do I listen to a lot of music?');
-
-if (music === 'yes' | music === 'y') {
-
-    tally = tally + 1;
-
-    alert ('Correct! Music is my favorite!');
-}
-
-else if (music === 'no' | music === 'n') {
-
-    alert ('What! Of course I do!');
-}
-
-var plants = prompt ('Do I keep a lot of plants in my house?').toLowerCase();
-console.log ('Do I keep a lot of plants in my house?');
-
-if (plants === 'yes' | plants === 'y') {
-    
-    tally = tally + 1;
-
-    alert ('Yes I do! Plants are the best.');
-}
-
-else if (plants === 'no' | plants === 'n') {
-
-    alert ('You are wrong! I love plants.');
-}
-
-var tears = prompt ('Was I born without tear ducts?').toLowerCase();
-console.log ('Was I born without tear ducts?');
-
-if (tears === 'yes' | tears === 'y') {
-
-    tally = tally + 1;
-
-    alert ('You are correct!');
-}
-
-else if (tears === 'no' | tears === 'n') {
-
-    alert ('As a matter of fact, I was born without tear ducts.');
-}
- 
 var states = ['idaho', 'utah', 'colorado'];
 
-
-
-for (  var i = 6 ; i > 0 ; i--) {
-   
-    var guess = prompt ( name + ', can you guess what state i\'ve lived in? You have ' + i + ' guesses.'.toLowerCase())
-   
+for ( var i = 6 ; i > 0 ; i--) {
+    var guess = prompt ( name + ', can you guess what state i\'ve lived in? You have ' + i + ' guesses.'.toLowerCase());
     if ( states.indexOf(guess) !== -1 ) {
         tally = tally + 1;
         alert ('Good job! Correct answers were: Idaho, Utah, and Colorado!'); 
@@ -116,7 +40,7 @@ var number = 25;
 
 for ( var i = 4 ; i > 0 ; i--) { 
 
-    var answer = prompt ('Now ' + name + ', can you guess my favorite number? You have ' + i + ' guesses.')
+    var answer = prompt ('Now ' + name + ', can you guess my favorite number? You have ' + i + ' guesses.');
 
     if ( answer > 25) {
         alert ('Too high!');
@@ -130,7 +54,6 @@ for ( var i = 4 ; i > 0 ; i--) {
 
         break;
     }
-    
     else {alert ('Too low!');}
 
 }
@@ -143,5 +66,3 @@ if ( tally > 4) {
 }
 
 else {alert ('Better Luck next time!');}
-
-
